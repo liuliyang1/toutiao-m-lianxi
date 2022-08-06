510c02c927b380eab170e648ecd532c8ed727eea
liuliyang <1690557749@qq.com>
2022-08-06 16:26:23 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <!-- 路由的出口 -->
     <router-view />
+    <i class="toutiao toutiao-shoucang"></i>
+    <van-cell title="单元格" is-link />
+    <van-cell title="单元格" is-link value="内容" />
+    <van-cell title="单元格" is-link arrow-direction="down" value="内容" />
+    <van-button loading type="primary" />
+    <van-button loading type="primary" loading-type="spinner" />
+    <van-button loading type="info" loading-text="加载中..." />
   </div>
 </template>
 <script>
